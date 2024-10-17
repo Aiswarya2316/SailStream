65bfacf616194f28a9f7714bcb8751533d865afc
Aiswarya2316 <aiswaryapinkey2gmail.com>
2024-10-17 10:40:36 +0530

    product

--- sailstream/src/component/Home.jsx
+++ sailstream/src/component/Home.jsx
@@ -1,11 +1,26 @@
-
-
 import img1 from '../assets/images.jpeg';
 import img2 from '../assets/041221_brows_embed-ee2c3c3b6d9a4129bbe6ee5665cc52ef.jpg';
 import img3 from '../assets/makeup-products-flat-lay-variety-laid-out-white-surface-including-lipsticks-eyeshadow-palettes-blush-brushes-other-328225355.webp';
-import Footer from './Footer';
+import React, { useState, useEffect } from "react";
+import  Footer from './Footer';
+import axios from 'axios';
 
-function Home() {
+const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [add, setAdd] = useState(false);
+  const [currentProduct, setCurrentProduct] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://apiecommerce.pythonanywhere.com/api/product/')
+      .then(response => setProducts(response.data))
+      .catch(error => console.log(error));
+  }, []);
+
+  const viewProduct = (product) => {
+    setAdd(true);
+    setCurrentProduct(product);
+  };
+
   return (
     <>
       <div id="demo" className="carousel slide" data-bs-ride="carousel">
@@ -17,13 +32,13 @@ function Home() {
 
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img src={img1} alt="Los Angeles" className="d-block" style={{ width: '100%', height: '50%'  }} />
+            <img src={img1} alt="Los Angeles" className="d-block" style={{ width: '100%', height: '50%' }} />
           </div>
           <div className="carousel-item">
-            <img src={img2} alt="Chicago" className="d-block" style={{ width: '100%' , height: '50%' }} />
+            <img src={img2} alt="Chicago" className="d-block" style={{ width: '75%', height: '25%' }} />
           </div>
           <div className="carousel-item">
-            <img src={img3} alt="New York" className="d-block" style={{ width: '100%' , height: '50%' }} />
+            <img src={img3} alt="New York" className="d-block" style={{ width: '100%', height: '50%' }} />
           </div>
         </div>
 
@@ -34,9 +49,26 @@ function Home() {
           <span className="carousel-control-next-icon"></span>
         </button>
       </div>
+
+      <div className="container mt-3">
+        <div className="row" id="a1">
+          {products.map(pro => (
+            <div className="col-lg-3 col-md-3 col-sm-6" key={pro.id}>
+              <div className="card" onClick={() => viewProduct(pro)}>
+                <img className="card-img-top img-fluid" src={pro.image} alt="product image" />
+                <div className="card-body">
+                  <h4 className='card-title'>{pro.p_name}</h4>
+                  <h6 className='card-text'>{pro.price}</h6>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
       <Footer/>
     </>
   );
-}
+};
 
 export default Home;
+
